cb3c25d782861f1891ecd749be9c8349661dea5b
Alex Patel <alexpatel@Alexs-MacBook-Pro.local>
2015-04-27 20:24:39 -0600

    format correct

---
+++ NumGen.java
@@ -0,0 +1,108 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+
+public class NumGen {
+	
+	FileWriter fw;
+	ArrayList<Integer> tasks;
+	ArrayList<Integer> machines;
+	Random rand = new Random();
+	static NumGen ng;
+	String input = "input.txt";
+	int t, n, m;
+	
+	
+	NumGen(int n, int m) throws IOException {
+		fw = new FileWriter(new File(input));
+		fw.write(n + "\n" + m + "\n");
+		
+		setTasks(genTasks(n));
+		setMachines(genMachines(m));
+		
+		ArrayList<Integer> times = getTasks();
+		ArrayList<Integer> speeds = getMachines();
+		
+		System.out.println("Tasks: ");
+		System.out.println("num: " + n);
+		displayList(times);
+		System.out.println("\nMachines: ");
+		System.out.println("num: " + m);
+		displayList(speeds);
+		
+		fw.close();
+	}
+	
+
+	public int numTasks() {
+		t = rand.nextInt(1000) + 1;
+		return t;
+	}
+	
+	public int numMachines() {
+		m = rand.nextInt(50) + 1;
+		return m;
+	}
+	
+	public ArrayList<Integer> genTasks(int num) {
+		tasks = new ArrayList<Integer>();
+		
+		for(int i = 0; i < num; i++) {
+			// task processing times in [1,10000]
+			int currentTask = rand.nextInt(10000) + 1; 
+			tasks.add(currentTask);
+		}
+		return tasks; 
+	}
+	
+	public ArrayList<Integer> genMachines(int num) {
+		machines = new ArrayList<Integer>();
+		
+		for(int i = 0; i < num; i++) {
+			// machine speeds in [1,20]
+			int currentMachine = rand.nextInt(20) + 1;
+			machines.add(currentMachine);
+		}
+		return machines;
+	}
+	
+	public void displayList(ArrayList<Integer> a) throws IOException {
+		for(int i = 0; i < a.size(); i++) {
+			fw.write(a.get(i) + " ");
+			System.out.print(a.get(i) + " ");
+		}
+		System.out.print("");
+		fw.write("\n");
+	}
+	
+	public ArrayList<Integer> getTasks() {return tasks;}
+
+	public void setTasks(ArrayList<Integer> tasks) {this.tasks = tasks;}
+
+	public ArrayList<Integer> getMachines() {return machines;}
+
+	public void setMachines(ArrayList<Integer> machines) {this.machines = machines;}
+	
+	public int getT() {return t;}
+
+	public void setT(int t) {this.t = t;}
+
+	public int getM() {return m;}
+
+	public void setM(int m) {this.m = m;}
+	
+	
+	
+	public static void main(String[] args) throws IOException {
+		if(args.length < 2)
+			System.out.println("Invalid arguments");
+		else {
+			int n = Integer.parseInt(args[0]); // tasks
+			int m = Integer.parseInt(args[1]); // machines
+			ng = new NumGen(n, m);
+		}
+	}
+}
---
+++ README.txt
@@ -0,0 +1,1 @@
+File: README.txt Author: Alex Patel NumGen.java generates random task times and machine speeds given the number of tasks and machines as parameters.To compile:$ javac NumGen.java To run:$ java NumGen 10 5where 10 is the number of task times you want to generate and 5 is the number ofmachine speeds you wan to generate 
